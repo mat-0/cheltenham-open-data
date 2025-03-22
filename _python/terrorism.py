364d16ca5ca0984113204f2bbee99db8b6e38997
mat-0 <Github@thechels.uk>
2025-03-22 23:29:03 +0000

    fix
    Signed-off-by: mat-0 <Github@thechels.uk>

--- _python/terrorism.py
+++ _python/terrorism.py
@@ -26,10 +26,10 @@ if __name__ == "__main__":
 
 
         string =  f'<h3>{level_class}</h3>\n'
-        string += f'<div class="{level_class}">\n\n'
+        string += f'<div class="{level_class}">\n'
         string += f'<ul>\n'
         string += f'<li>{level}</li>\n'
-        string += f'<li>It has been {days_since_update} days since the last change ({update})</li>\n\n'
+        string += f'<li>It has been {days_since_update} days since the last change ({update})</li>\n'
         string += f'<li>Details: {desc}</li>\n'
         string += f'</ul>\n'
         string += f'</div>\n'
