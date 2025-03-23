a06be7c71f18c298d07f0073658d1464bf9f2af6
mat-0 <Github@thechels.uk>
2025-03-23 13:30:58 +0000

    add weather
    Signed-off-by: mat-0 <Github@thechels.uk>

---
+++ _python/weather.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import os
+import helper
+import datetime
+import pathlib
+
+API_KEY = os.getenv("OPEN_WEATHER_KEY") or ''
+CITY = 'Cheltenham'
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+url = f"{BASE_URL}?q={CITY}&appid={API_KEY}&units=metric"
+
+if __name__ == "__main__":
+    try:
+        root = pathlib.Path(__file__).parent.parent.resolve()
+        response = requests.get(url)
+        response_dict = json.loads(response.text)
+
+        if response_dict["cod"] != 200:
+            string_today = "- Weather data not available"
+        else:
+            output_date = datetime.date.today().strftime("%A, %d %B %Y")
+
+            day_temp = str(response_dict["main"]["temp"])
+            feels_like = str(response_dict["main"]["feels_like"])
+            day_desc = str(response_dict["weather"][0]["description"])
+            high_temp = str(response_dict["main"]["temp_max"])
+            low_temp = str(response_dict["main"]["temp_min"])
+            wind_speed = str(response_dict["wind"]["speed"])
+            visibility = str(response_dict["visibility"])
+            pressure = str(response_dict["main"]["pressure"])
+            humidity = str(response_dict["main"]["humidity"])
+
+            sunrise = datetime.datetime.fromtimestamp(response_dict["sys"]["sunrise"]).strftime("%H:%M")
+            sunset = datetime.datetime.fromtimestamp(response_dict["sys"]["sunset"]).strftime("%H:%M")
+
+            string_today = f"### On {output_date}\n\n"
+            string_today += f"- The average temperature today is {day_temp}˚C,\n"
+            string_today += f"- With highs of {high_temp}˚C and lows of {low_temp}˚C,\n"
+            string_today += f"- It may feel like {feels_like}˚C with {day_desc}\n"
+            string_today += f"- The wind speed is {wind_speed}m/s and visibility is {visibility}m\n"
+            string_today += f"- The pressure is {pressure}hPa and humidity is {humidity}%\n"
+            string_today += f"- The sun will rise at {sunrise} and set at {sunset}\n"
+
+        f = root / "index.md"
+        m = f.open().read()
+        c = helper.replace_chunk(m, "weather_marker", string_today)
+        f.open("w").write(c)
+        print("Weather completed")
+
+    except FileNotFoundError:
+        print("File does not exist, unable to proceed")
--- index.md
+++ index.md
@@ -8,10 +8,16 @@ seo: Menu
 ![Logo](/images/logo.jpg){:width="832px"}
 
 
-## Today's weather
-
 <!-- weather_marker starts -->
-- coming soon
+### On Sunday, 23 March 2025
+
+- The average temperature today is 9.67˚C,
+- With highs of 11.03˚C and lows of 9.26˚C,
+- It may feel like 7.79˚C with drizzle
+- The wind speed is 3.6m/s and visibility is 1800m
+- The pressure is 1005hPa and humidity is 94%
+- The sun will rise at 06:03 and set at 18:25
+
 <!-- weather_marker ends -->
 
 
