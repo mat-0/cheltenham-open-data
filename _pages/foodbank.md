d29efe12e2856df775d8820677223fb7125e1c84
mat-0 <Github@thechels.uk>
2025-03-23 13:50:31 +0000

    updates
    Signed-off-by: mat-0 <Github@thechels.uk>

--- _pages/foodbank.md
+++ _pages/foodbank.md
@@ -7,7 +7,7 @@ permalink: /foodbank
 
 ---
 
-<!-- summary_marker starts -->
+<!-- summary1_marker starts -->
 ## List of needed items in Cheltenham
 
 Last updated: 2025-02-12
@@ -19,16 +19,42 @@ Last updated: 2025-02-12
 - Desserts - Hot Or Cold, Packets Or Cans
 - Deodorants - Ladies And Men's
 - Nappies - Size 4
-<!-- summary_marker ends -->
+<!-- summary1_marker ends -->
 
 [shopping list](https://cheltenham.foodbank.org.uk/give-help/donate-food/)
 
 ### Contact Details
 
-<!-- contact_marker starts -->
+<!-- contact1_marker starts -->
 - Email: foodbank@cheltenhamelim.org
 - Tel: 01242570080
 - Address: Cheltenham Elim Church 115-117 St George's Road Cheltenham GL50 3ED
 - Network: Trussell
 - Charity number: [251549](https://register-of-charities.charitycommission.gov.uk/charity-details/?regid=251549&subid=0)
-<!-- contact_marker ends -->
+<!-- contact1_marker ends -->
+
+<!-- summary2_marker starts -->
+## List of needed items in Cheltenham
+
+Last updated: 2025-02-12
+
+- Tinned Cold Meat (Or Fish)
+- Pasta Sauce
+- Washing Up Liquid, Washing Powder (Powder Or Capsules Please, Not Liquid)
+- Soup - Packets Or Tins
+- Desserts - Hot Or Cold, Packets Or Cans
+- Deodorants - Ladies And Men's
+- Nappies - Size 4
+<!-- summary2_marker ends -->
+
+[shopping list](https://cheltenham.foodbank.org.uk/give-help/donate-food/)
+
+### Contact Details
+
+<!-- contact2_marker starts -->
+- Email: foodbank@cheltenhamelim.org
+- Tel: 01242570080
+- Address: Cheltenham Elim Church 115-117 St George's Road Cheltenham GL50 3ED
+- Network: Trussell
+- Charity number: [251549](https://register-of-charities.charitycommission.gov.uk/charity-details/?regid=251549&subid=0)
+<!-- contact2_marker ends -->
--- _python/foodbank.py
+++ _python/foodbank.py
@@ -2,13 +2,11 @@
 import helper
 import pathlib
 import json
-from requests import get
-import os
 
 
 if __name__ == "__main__":
     root = pathlib.Path(__file__).parent.parent.resolve()
-    with open(root / "_data/foodbank.json", 'r') as filehandler:
+    with open(root / "_data/foodbank-cheltenham.json", 'r') as filehandler:
         data = json.load(filehandler)
         needs = data['need']['needs']
         date = helper.date_to_iso(data['need']['created'])
@@ -25,7 +23,28 @@ if __name__ == "__main__":
 
     md = root / "_pages/foodbank.md"
     md_contents = md.open().read()
-    md_contents = helper.replace_chunk(md_contents,"summary_marker",output)
-    md_contents = helper.replace_chunk(md_contents,"contact_marker",contact_string)
+    md_contents = helper.replace_chunk(md_contents,"summary1_marker",output)
+    md_contents = helper.replace_chunk(md_contents,"contact1_marker",contact_string)
+    md.open("w").write(md_contents)
+
+    with open(root / "_data/foodbank-cheltenham-open-door.json", 'r') as filehandler:
+        data = json.load(filehandler)
+        needs = data['need']['needs']
+        date = helper.date_to_iso(data['need']['created'])
+        output = f"## List of needed items in Cheltenham Open Door\n\n"
+        output += f"Last updated: {date}\n\n"
+        output += f"- {needs}".replace("\n", "\n- ")
+        output.rstrip("-")
+        address = data['address'].replace("\r\n"," ")
+        contact_string = (f"- Email: {data['email']}\n"
+                        f"- Tel: {data['phone']}\n"
+                        f"- Address: {address}\n"
+                        f"- Network: {data['network']}\n"
+                        f"- Charity number: [{data['charity']['registration_id']}]({data['charity']['register_url']})")
+
+    md = root / "_pages/foodbank.md"
+    md_contents = md.open().read()
+    md_contents = helper.replace_chunk(md_contents,"summary2_marker",output)
+    md_contents = helper.replace_chunk(md_contents,"contact2_marker",contact_string)
     md.open("w").write(md_contents)
 
